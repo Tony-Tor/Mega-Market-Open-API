c4df5f81ead2dc5879e56a1bb37523d1bd927dd9
Tony-Tor <Tony.Tor2016@yandex.ru>
2022-06-23 22:51:18 +0400

    Initial commit

---
+++ src/main/java/com/example/store/Mega/Market/Open/API/MegaMarketOpenApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.store.Mega.Market.Open.API;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MegaMarketOpenApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MegaMarketOpenApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/store/Mega/Market/Open/API/MegaMarketOpenApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.store.Mega.Market.Open.API;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MegaMarketOpenApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
